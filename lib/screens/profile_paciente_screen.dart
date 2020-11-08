e2783ced1e3e419a54aed8eec1c97c969d79a3d3
trabalhogise <gisetoledo83@outlook.com>
2020-11-07 21:06:21 -0300

    Ajustes tela de entrada do paciente

--- lib/screens/profile_paciente_screen.dart
+++ lib/screens/profile_paciente_screen.dart
@@ -12,48 +12,74 @@ class ProfilePacienteScreen extends StatefulWidget {
 class _ProfilePacienteScreenState extends State<ProfilePacienteScreen> {
   @override
   Widget build(BuildContext context) {
+    final _screenSize = MediaQuery.of(context).size;
     return Container(
       padding: const EdgeInsets.fromLTRB(32, 24, 16, 8),
-      height: 180,
       child: Consumer<UserManagerPaciente>(
         builder: (_, userManager, __) {
           return Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: <Widget>[
-              Text(
-                'Meu cadastro',
-                style: TextStyle(
-                  fontSize: 34,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Text(
-                'Olá, ${userManager.user?.name ?? ''}',
-                overflow: TextOverflow.ellipsis,
-                maxLines: 2,
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              GestureDetector(
-                onTap: () {
-                  if (userManager.isLoggedIn) {
-                    userManager.signOut();
-                  } else {
-                    Navigator.of(context).pushNamed('pacientelogin');
-                  }
-                },
+              Padding(
+                padding: const EdgeInsets.all(8.0),
                 child: Text(
-                  userManager.isLoggedIn ? 'Sair' : 'Entre ou cadastre-se >',
+                  'Área do paciente',
                   style: TextStyle(
-                    color: Theme.of(context).primaryColor,
-                    fontSize: 16,
+                    fontSize: 34,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
               ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'Olá, ${userManager.user?.name ?? 'Visitante'}',
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: GestureDetector(
+                  onTap: () {
+                    if (userManager.isLoggedIn) {
+                      userManager.signOut();
+                    } else {
+                      Navigator.of(context).pushNamed('pacientelogin');
+                    }
+                  },
+                  child: Text(
+                    userManager.isLoggedIn ? 'Sair' : 'Entre ou cadastre-se >',
+                    style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              userManager.isLoggedIn
+                  ? SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      child: RaisedButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, 'sessaopaciente');
+                        },
+                        child: Text(
+                          "Minhas sessões",
+                          style: TextStyle(
+                              color: Color.fromRGBO(255, 51, 1, 1),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20),
+                        ),
+                      ),
+                    )
+                  : Container(),
             ],
           );
         },
