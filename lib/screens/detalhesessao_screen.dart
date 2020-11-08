9468171055f08edb42d3ea19953bee8a85140745
trabalhogise <gisetoledo83@outlook.com>
2020-11-08 03:46:10 -0300

    Correção dos dados do paciente

--- lib/screens/detalhesessao_screen.dart
+++ lib/screens/detalhesessao_screen.dart
@@ -5,8 +5,6 @@ import 'package:url_launcher/url_launcher.dart';
 import '../models/sessao.dart';
 import '../providers/sessao_provider.dart';
 
-
-
 class DetalheSessaoScreen extends StatefulWidget {
   final Sessao sessao;
 
@@ -18,12 +16,13 @@ class DetalheSessaoScreen extends StatefulWidget {
 }
 
 class _DetalheSessaoScreenState extends State<DetalheSessaoScreen> {
+  Future<void> _launched;
   @override
   Widget build(BuildContext context) {
     final sessaoProvider = Provider.of<SessaoProvider>(context);
-     var _launchUrl = widget.sessao.exerciseUrl;
+    var _launchUrl = widget.sessao.exerciseUrl;
 
-   Future<void> _launchInBrowser(String url) async {
+    Future<void> _launchInBrowser(String url) async {
       if (await canLaunch(url)) {
         await launch(
           url,
@@ -37,34 +36,94 @@ class _DetalheSessaoScreenState extends State<DetalheSessaoScreen> {
     }
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Sessão'),
+      appBar: AppBar(
+        title: Text('Sessão'),
+      ),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            FutureBuilder(
+              future: _launched,
+              builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
+                if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                } else {
+                  return Center(
+                    child: Container(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Nome: ${widget.sessao.pacienteName}',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            'CPF: ${widget.sessao.pacienteCpf}',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            'Fisioterapeuta: ${widget.sessao.fisioName}',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            'Número de sessões: ${widget.sessao.numerosessoes.toString()}',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            'Instruções: ${widget.sessao.description}',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                }
+              },
+            ),
+            Container(
+              margin: const EdgeInsets.all(8),
+              child: Column(
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    child: RaisedButton(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(38.0),
+                      ),
+                      child: const Text(
+                        'Assistir ao vídeo',
+                        style:
+                            TextStyle(color: Color.fromRGBO(255, 255, 255, 1)),
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _launched = _launchInBrowser(_launchUrl);
+                        });
+                      },
+                      color: Theme.of(context).primaryColor,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
-        body: FutureBuilder(
-                future: _launched,
-                builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
-                  if (snapshot.hasError) {
-                    return Text('Error: ${snapshot.error}');
-                  } else {
-                    return Container(
-                      child: Text(widget.sessao.pacienteName),
-                    );
-                  }
-                },
-              ),
-              Container(
-          child: Column(
-            children: [
-               RaisedButton(
-                child: const Text('Launch In App'),
-                onPressed: () {
-                  setState(() {
-                    _launched = _launchInBrowser(_launchUrl);
-                  });
-                },
-              ),
-            ],
-          ),
-        ),);
+      ),
+    );
   }
 }
