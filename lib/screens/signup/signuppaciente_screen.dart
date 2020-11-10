49f8d15627f4237cc67f9b1505a904b55614a44f
trabalhogise <gisetoledo83@outlook.com>
2020-11-10 12:21:23 -0300

    Correção bug layout size

--- lib/screens/signup/signuppaciente_screen.dart
+++ lib/screens/signup/signuppaciente_screen.dart
@@ -35,170 +35,177 @@ class _SignUpPacienteScreenState extends State<SignUpPacienteScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Form(
-                  key: _formKey,
-                  child: Column(
-                    children: <Widget>[
-                      TextFormField(
-                        decoration:
-                            const InputDecoration(hintText: 'Nome Completo'),
-                        validator: (name) {
-                          if (name.isEmpty)
-                            return 'Campo obrigatório';
-                          else if (name.trim().split(' ').length <= 1)
-                            return 'Preencha seu nome completo';
-                          return null;
-                        },
-                        onSaved: (name) => user.name = name,
-                      ),
-                      const SizedBox(
-                        height: 16,
-                      ),
-                      TextFormField(
-                        decoration: const InputDecoration(hintText: 'CPF'),
-                        validator: (cpf) {
-                          if (cpf.isEmpty) return 'Campo obrigatório';
-                          return null;
-                        },
-                        onSaved: (cpf) => user.cpf = cpf,
-                      ),
-                      const SizedBox(
-                        height: 16,
-                      ),
-                      TextFormField(
-                        decoration: const InputDecoration(hintText: 'E-mail'),
-                        keyboardType: TextInputType.emailAddress,
-                        validator: (email) {
-                          if (email.isEmpty)
-                            return 'Campo obrigatório';
-                          else if (!emailValid(email)) return 'E-mail inválido';
-                          return null;
-                        },
-                        onSaved: (email) => user.email = email,
-                      ),
-                      const SizedBox(
-                        height: 16,
-                      ),
-                      TextFormField(
-                        decoration: const InputDecoration(
-                            hintText: 'Número de telefone'),
-                        validator: (telephone) {
-                          if (telephone.isEmpty) return 'Campo obrigatório';
-                          return null;
-                        },
-                        onSaved: (telephone) =>
-                            user.telephone = int.parse(telephone),
-                      ),
-                      const SizedBox(
-                        height: 16,
-                      ),
-                      TextFormField(
-                        decoration: const InputDecoration(hintText: 'Senha'),
-                        obscureText: true,
-                        validator: (pass) {
-                          if (pass.isEmpty)
-                            return 'Campo obrigatório';
-                          else if (pass.length < 6) return 'Senha muito curta';
-                          return null;
-                        },
-                        onSaved: (pass) => user.password = pass,
-                      ),
-                      const SizedBox(
-                        height: 16,
-                      ),
-                      TextFormField(
-                        decoration:
-                            const InputDecoration(hintText: 'Repita a Senha'),
-                        obscureText: true,
-                        validator: (pass) {
-                          if (pass.isEmpty)
-                            return 'Campo obrigatório';
-                          else if (pass.length < 6) return 'Senha muito curta';
-                          return null;
-                        },
-                        onSaved: (pass) => user.confirmPassword = pass,
-                      ),
-                      const SizedBox(
-                        height: 16,
-                      ),
-                      SizedBox(
-                        height: 44,
-                        width: MediaQuery.of(context).size.width * 0.8,
-                        child: RaisedButton(
-                          color: Theme.of(context).primaryColor,
-                          disabledColor:
-                              Theme.of(context).primaryColor.withAlpha(100),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: new BorderRadius.circular(38.0),
-                          ),
-                          textColor: Colors.white,
-                          onPressed: () {
-                            if (_formKey.currentState.validate()) {
-                              _formKey.currentState.save();
-                              if (user.password != user.confirmPassword) {
-                                scaffoldKey.currentState.showSnackBar(
-                                  SnackBar(
-                                    content: Text('Senhas não são idênticas!'),
-                                    backgroundColor: Colors.red,
-                                  ),
-                                );
-                                return;
-                              }
-                              context.read<UserManagerPaciente>().signUp(
-                                    userfb: user,
-                                    onSuccess: () {
-                                      Navigator.of(context)
-                                          .pushNamed('pacientelogin');
-                                      debugPrint('sucesso');
-                                    },
-                                    onFail: (e) {
-                                      scaffoldKey.currentState.showSnackBar(
-                                        SnackBar(
-                                          content:
-                                              Text('Falha ao cadastrar: $e'),
-                                          backgroundColor: Colors.red,
-                                        ),
-                                      );
-                                    },
-                                  );
-                            }
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Form(
+                    key: _formKey,
+                    child: Column(
+                      children: <Widget>[
+                        TextFormField(
+                          decoration:
+                              const InputDecoration(hintText: 'Nome Completo'),
+                          validator: (name) {
+                            if (name.isEmpty)
+                              return 'Campo obrigatório';
+                            else if (name.trim().split(' ').length <= 1)
+                              return 'Preencha seu nome completo';
+                            return null;
                           },
-                          padding: const EdgeInsets.fromLTRB(24, 12, 24, 12),
-                          child: const Text(
-                            'Criar Conta',
-                            style: TextStyle(fontSize: 18),
-                          ),
+                          onSaved: (name) => user.name = name,
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: SizedBox(
+                        const SizedBox(
+                          height: 16,
+                        ),
+                        TextFormField(
+                          decoration: const InputDecoration(hintText: 'CPF'),
+                          validator: (cpf) {
+                            if (cpf.isEmpty) return 'Campo obrigatório';
+                            return null;
+                          },
+                          onSaved: (cpf) => user.cpf = cpf,
+                        ),
+                        const SizedBox(
+                          height: 16,
+                        ),
+                        TextFormField(
+                          decoration: const InputDecoration(hintText: 'E-mail'),
+                          keyboardType: TextInputType.emailAddress,
+                          validator: (email) {
+                            if (email.isEmpty)
+                              return 'Campo obrigatório';
+                            else if (!emailValid(email))
+                              return 'E-mail inválido';
+                            return null;
+                          },
+                          onSaved: (email) => user.email = email,
+                        ),
+                        const SizedBox(
+                          height: 16,
+                        ),
+                        TextFormField(
+                          decoration: const InputDecoration(
+                              hintText: 'Número de telefone'),
+                          validator: (telephone) {
+                            if (telephone.isEmpty) return 'Campo obrigatório';
+                            return null;
+                          },
+                          onSaved: (telephone) =>
+                              user.telephone = int.parse(telephone),
+                        ),
+                        const SizedBox(
+                          height: 16,
+                        ),
+                        TextFormField(
+                          decoration: const InputDecoration(hintText: 'Senha'),
+                          obscureText: true,
+                          validator: (pass) {
+                            if (pass.isEmpty)
+                              return 'Campo obrigatório';
+                            else if (pass.length < 6)
+                              return 'Senha muito curta';
+                            return null;
+                          },
+                          onSaved: (pass) => user.password = pass,
+                        ),
+                        const SizedBox(
+                          height: 16,
+                        ),
+                        TextFormField(
+                          decoration:
+                              const InputDecoration(hintText: 'Repita a Senha'),
+                          obscureText: true,
+                          validator: (pass) {
+                            if (pass.isEmpty)
+                              return 'Campo obrigatório';
+                            else if (pass.length < 6)
+                              return 'Senha muito curta';
+                            return null;
+                          },
+                          onSaved: (pass) => user.confirmPassword = pass,
+                        ),
+                        const SizedBox(
+                          height: 16,
+                        ),
+                        SizedBox(
                           height: 44,
                           width: MediaQuery.of(context).size.width * 0.8,
                           child: RaisedButton(
                             color: Theme.of(context).primaryColor,
                             disabledColor:
                                 Theme.of(context).primaryColor.withAlpha(100),
-                            textColor: Colors.white,
                             shape: RoundedRectangleBorder(
                               borderRadius: new BorderRadius.circular(38.0),
                             ),
+                            textColor: Colors.white,
                             onPressed: () {
-                              Navigator.of(context)
-                                  .pushReplacementNamed('pacientelogin');
+                              if (_formKey.currentState.validate()) {
+                                _formKey.currentState.save();
+                                if (user.password != user.confirmPassword) {
+                                  scaffoldKey.currentState.showSnackBar(
+                                    SnackBar(
+                                      content:
+                                          Text('Senhas não são idênticas!'),
+                                      backgroundColor: Colors.red,
+                                    ),
+                                  );
+                                  return;
+                                }
+                                context.read<UserManagerPaciente>().signUp(
+                                      userfb: user,
+                                      onSuccess: () {
+                                        Navigator.of(context)
+                                            .pushNamed('pacientelogin');
+                                        debugPrint('sucesso');
+                                      },
+                                      onFail: (e) {
+                                        scaffoldKey.currentState.showSnackBar(
+                                          SnackBar(
+                                            content:
+                                                Text('Falha ao cadastrar: $e'),
+                                            backgroundColor: Colors.red,
+                                          ),
+                                        );
+                                      },
+                                    );
+                              }
                             },
                             padding: const EdgeInsets.fromLTRB(24, 12, 24, 12),
                             child: const Text(
-                              'Login',
+                              'Criar Conta',
                               style: TextStyle(fontSize: 18),
                             ),
                           ),
                         ),
-                      )
-                    ],
-                  )),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: SizedBox(
+                            height: 44,
+                            width: MediaQuery.of(context).size.width * 0.8,
+                            child: RaisedButton(
+                              color: Theme.of(context).primaryColor,
+                              disabledColor:
+                                  Theme.of(context).primaryColor.withAlpha(100),
+                              textColor: Colors.white,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: new BorderRadius.circular(38.0),
+                              ),
+                              onPressed: () {
+                                Navigator.of(context)
+                                    .pushReplacementNamed('pacientelogin');
+                              },
+                              padding:
+                                  const EdgeInsets.fromLTRB(24, 12, 24, 12),
+                              child: const Text(
+                                'Login',
+                                style: TextStyle(fontSize: 18),
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    )),
+              ),
             )
           ],
         ),
