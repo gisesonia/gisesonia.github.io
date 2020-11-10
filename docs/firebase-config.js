f6cf74b747e55d4e1dac67fbc0c6904cfe542cea
trabalhogise <gisetoledo83@outlook.com>
2020-11-10 12:32:35 -0300

    Arquivos para rodar no browser

---
+++ docs/firebase-config.js
@@ -0,0 +1,11 @@
+export var firebaseConfig = {
+    apiKey: "AIzaSyBNL6NjZEABO1cVTIKZSmHGTjPBAB5thM4",
+    authDomain: "crud-provider.firebaseapp.com",
+    databaseURL: 'https://crud-provider.firebaseio.com',
+    projectId: "crud-firestore-d4ac8",
+    storageBucket: "crud-firestore.appspot.com",
+    messagingSenderId:"",
+    appId:'1:20280568558:android:0ac289730ca31dc4f561ff',
+    measurementId: "G-XXXXXXXXXX"
+};
+//Fica em configuração do aplicativo, na engrenagem ao lado do nome do app
